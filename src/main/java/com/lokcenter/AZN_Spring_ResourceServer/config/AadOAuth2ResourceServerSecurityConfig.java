8c106f83d5742b364ba94b28522d5fb89726cd76
chfle <christian.lehnert.home@gmail.com>
2022-06-30 02:15:05 -0400

    enable cors

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/config/AadOAuth2ResourceServerSecurityConfig.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/config/AadOAuth2ResourceServerSecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
 
 /**
  * Resource Server with Microsoft AAD
@@ -22,7 +26,22 @@ public class AadOAuth2ResourceServerSecurityConfig extends AadResourceServerWebS
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        // allow only authenticated users
+        // needed to get post request
+        http.csrf().disable();
+
+        //
+        http.cors(c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(
+                        List.of("http://localhost:8880"));
+                config.setAllowedMethods(
+                        List.of("GET", "POST", "PUT", "DELETE"));
+                return config;
+            };
+            c.configurationSource(source);
+        });
+
         http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
     }
 }
