b59bd03e579ec6f6fff711867a47e5d9f1d9f5cb
chfle <christian.lehnert.home@gmail.com>
2022-07-01 04:21:52 -0400

    basic dayplan endpoint

---
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/DayPlanController.java
@@ -0,0 +1,22 @@
+package com.lokcenter.AZN_Spring_ResourceServer.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * Dayplan endpoint
+ *
+ * @version 1.01 - 01-07-2022
+ */
+
+@RestController
+@RequestMapping("/dayplan")
+@PreAuthorize("hasAuthority('SCOPE_UserApi.Read')")
+public class DayPlanController {
+    @PostMapping()
+    boolean postDayPlan(@RequestBody Map<String, Object> data) {
+        return true;
+    }
+}
