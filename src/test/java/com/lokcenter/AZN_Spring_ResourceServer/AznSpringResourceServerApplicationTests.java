3f7e635920762c3d4ff38490fc5be7540815ef35
chfle <christian.lehnert.home@gmail.com>
2022-07-17 17:54:33 +0200

    fake database for testing & surefire

--- src/test/java/com/lokcenter/AZN_Spring_ResourceServer/AznSpringResourceServerApplicationTests.java
+++ src/test/java/com/lokcenter/AZN_Spring_ResourceServer/AznSpringResourceServerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.lokcenter.AZN_Spring_ResourceServer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AznSpringResourceServerApplicationTests {
 
 	@Test
