b1ce4d8b2d3d50b12e2e75bedbc2b7a065d34af1
chfle <christian.lehnert.home@gmail.com>
2022-07-18 10:02:52 +0200

    first login testing

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/LoginController.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/LoginController.java
@@ -5,6 +5,8 @@ import com.lokcenter.AZN_Spring_ResourceServer.database.sql.User;
 import com.lokcenter.AZN_Spring_ResourceServer.helper.JunitHelper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,30 +33,36 @@ public class LoginController {
 
     @PostMapping
     @PreAuthorize("hasAuthority('SCOPE_UserApi.Write')")
-    boolean postLogin(@RequestBody Map<String, Object> payload) {
+    ResponseEntity<Boolean> postLogin(@RequestBody Map<String, Object> payload) {
         User user = new User();
 
         try {
             if (payload.containsKey("username") && payload.containsKey("firstLogin")) {
 
+                System.out.println(payload.get("firstLogin"));
                 user.setUsername((String) payload.get("username"));
 
-                // convert back to a utils.date to use sql.date
+                // convert back to an utils.date to use sql.date
                 var dateFormat = new SimpleDateFormat("dd/MMM/yyyy");
                 var utilsDate =dateFormat.parse((String)payload.get("firstLogin"));
 
                 user.setFirstLogin(new java.sql.Date(utilsDate.getTime()));
+            } else {
+               throw new Exception("Bad request");
             }
         } catch (Exception exception) {
             exception.printStackTrace();
-            return false;
+            return new ResponseEntity<Boolean>(false, HttpStatus.BAD_REQUEST);
         }
 
-        // only set if user does not exists
-       if (userRepository.findByUsername(user.getUsername()).isEmpty()) {
-           userRepository.save(user);
-       }
+        // check if not junit
+        if (!JunitHelper.isJUnitTest())  {
+            // only set if user does not exist
+            if (userRepository.findByUsername(user.getUsername()).isEmpty()) {
+                userRepository.save(user);
+            }
+        }
 
-       return true;
+       return new ResponseEntity<>(true, HttpStatus.OK);
     }
 }
---
+++ src/test/java/com/lokcenter/AZN_Spring_ResourceServer/controller/LoginControllerTest.java
@@ -0,0 +1,109 @@
+package com.lokcenter.AZN_Spring_ResourceServer.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LoginControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    private static final MediaType APPLICATION_JSON_UTF8 =
+            new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(),
+                    StandardCharsets.UTF_8);
+
+
+    @Test
+    @DisplayName("/login - post should only work with SCOPE_UserApi.Write permission")
+    @WithMockUser(authorities = {"SCOPE_UserApi.Write"})
+    void loginPost_should_only_allow_write_permission() throws Exception {
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("test", "test");
+
+        mvc.perform(post("/login")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(data))
+                        .header("Origin","http://localhost:8880"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("/login - post should not work with SCOPE_UserApi.Read permission")
+    @WithMockUser(authorities = {"SCOPE_UserApi.Read"})
+    void loginPost_should_not_work_with_read_permission() throws Exception {
+        Map<String, Object> data = new HashMap<>();
+
+        data.put("test", "test");
+
+        mvc.perform(post("/login")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(data))
+                        .header("Origin","http://localhost:8880"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("/login - post should work with good and bad payload")
+    @WithMockUser(authorities = {"SCOPE_UserApi.Write"})
+    void loginPost_with_good_and_bad_payload() throws Exception{
+        // Test with no payload
+        mvc.perform(post("/login")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(new HashMap<String, Object>()))
+                        .header("Origin","http://localhost:8880"))
+                .andExpect(status().isBadRequest());
+
+        // Test with wrong payload
+        mvc.perform(post("/login")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper()
+                                .writeValueAsString(Map.of("password", "23jlkjewr", "username", "chris")))
+                        .header("Origin","http://localhost:8880"))
+                .andExpect(status().isBadRequest());
+
+        // test with right payload
+        mvc.perform(post("/login")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper()
+                                .writeValueAsString(Map.of("firstLogin", "18/Jul/2022", "username", "chris")))
+                        .header("Origin","http://localhost:8880"))
+                .andExpect(status().isOk());
+
+        // test with more payload as expected
+        mvc.perform(post("/login")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper()
+                                .writeValueAsString(
+                                        Map.of(
+                                                "firstLogin", "18/Jul/2022",
+                                                "username", "chris",
+                                                "password", "chris")))
+                        .header("Origin","http://localhost:8880"))
+                .andExpect(status().isOk());
+
+        // test with wrong formatted payload
+        mvc.perform(post("/login")
+                        .contentType(APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper()
+                                // date is wrong
+                                .writeValueAsString(Map.of("firstLogin", "18/43/20", "username", "chris")))
+                        .header("Origin","http://localhost:8880"))
+                .andExpect(status().isBadRequest());
+    }
+}
