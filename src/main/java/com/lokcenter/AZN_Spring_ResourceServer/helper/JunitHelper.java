2d647ef1a90802e76797d973de2ed69ef36b9fbe
chfle <christian.lehnert.home@gmail.com>
2022-07-30 10:45:20 +0200

    javadoc

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/helper/JunitHelper.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/helper/JunitHelper.java
@@ -1,6 +1,18 @@
 package com.lokcenter.AZN_Spring_ResourceServer.helper;
 
+/**
+ * Junit Helper
+ *
+ * Checks where this code is executed
+ *
+ * @version 1.2 2022-07-30
+ */
 public class JunitHelper {
+    /**
+     * Checks current environment
+     *
+     * @return boolean
+     */
     public static boolean isJUnitTest() {
         for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
             if (element.getClassName().startsWith("org.junit.")) {
