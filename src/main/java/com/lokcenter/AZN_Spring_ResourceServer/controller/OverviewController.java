5669145e08ae9bb86c9210d6498ad713e3ed5a1d
chfle <christian.lehnert.home@gmail.com>
2022-10-12 00:42:45 -0700

    bug fix

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/OverviewController.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/OverviewController.java
@@ -1,5 +1,8 @@
 package com.lokcenter.AZN_Spring_ResourceServer.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lokcenter.AZN_Spring_ResourceServer.database.tables.DayPlanData;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,14 +15,19 @@ public class OverviewController {
     @GetMapping
     @PreAuthorize("hasAuthority('SCOPE_UserApi.Read')")
     String getOverviewByMonth(
-            @RequestParam(name = "firstday", required = true) String firstDay,
-            @RequestParam(name = "lastday", required = true) String lastDay,
+            @RequestParam(name = "firstday", required = false) String firstDay,
+            @RequestParam(name = "lastday", required = false) String lastDay,
             @RequestParam(name = "role", required = true) String role,
-            @RequestParam(name = "userId", required = false) String userId) {
+            @RequestParam(name = "userId", required = false) String userId,
+            @RequestParam(name = "month", required = false) String month) throws JsonProcessingException {
 
         // TODO: Get Role
         // TODO: Query Tables and pack it into one JSON BLOB
 
-        return "";
+
+
+        return new ObjectMapper().writer().
+                withDefaultPrettyPrinter()
+                .writeValueAsString(new DayPlanData());
     }
 }
