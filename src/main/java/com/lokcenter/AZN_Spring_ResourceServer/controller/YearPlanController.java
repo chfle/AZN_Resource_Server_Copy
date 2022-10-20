215df11db2cefdd8153fd75178c3298cddf28967
chfle <christian.lehnert.home@gmail.com>
2022-10-19 22:52:39 -0700

    get basic user date for yearplan

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/YearPlanController.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/YearPlanController.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lokcenter.AZN_Spring_ResourceServer.database.repository.UserRepository;
 import com.lokcenter.AZN_Spring_ResourceServer.database.tables.DayPlanData;
 import com.lokcenter.AZN_Spring_ResourceServer.database.tables.Users;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Time;
 import java.util.Optional;
 
 @RestController
@@ -20,10 +25,23 @@ public class YearPlanController {
     @Autowired
     private UserRepository userRepository;
 
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private class YearPlan {
+        @Setter
+        @Getter
+        private String FullName;
+
+        @Setter
+        @Getter
+        private Time DailyWorkTime;
+    }
+
     @GetMapping
     @PreAuthorize("hasAuthority('SCOPE_UserApi.Read')")
     String getYearPlanByUser(Authentication auth) throws Exception {
         Jwt jwt = (Jwt) auth.getPrincipal();
+
 
         String name = jwt.getClaim("unique_name");
 
@@ -31,9 +49,16 @@ public class YearPlanController {
         Optional<Users> user = userRepository.findByUsername(name);
 
         if (user.isPresent()) {
+            // get realname from user
+            String realname = jwt.getClaim("name");
+
+            var yearPlanCurrent = new YearPlan();
+
+            yearPlanCurrent.setFullName(realname);
+
             return new ObjectMapper().writer().
                     withDefaultPrettyPrinter()
-                    .writeValueAsString(new DayPlanData());
+                    .writeValueAsString(yearPlanCurrent);
         }
 
         throw new Exception("Bad Request");
