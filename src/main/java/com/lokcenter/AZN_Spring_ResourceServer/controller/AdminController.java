88f73aed5f0d66d98188f035adceb48de6d4a12d
chfle <christian.lehnert.home@gmail.com>
2022-11-09 00:51:10 -0800

    get username to admin panel

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/AdminController.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/AdminController.java
@@ -1,11 +1,18 @@
 package com.lokcenter.AZN_Spring_ResourceServer.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lokcenter.AZN_Spring_ResourceServer.database.repository.UserInfoRepository;
+import com.lokcenter.AZN_Spring_ResourceServer.database.repository.UserRepository;
+import com.lokcenter.AZN_Spring_ResourceServer.database.tables.UserInfo;
+import com.lokcenter.AZN_Spring_ResourceServer.database.tables.Users;
+import com.lokcenter.AZN_Spring_ResourceServer.helper.ds.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * Admin Controller
@@ -14,6 +21,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserInfoRepository userInfoRepository;
     /**
      * Get all user data needed for the admin panel
      * @return json reprenstation of data
@@ -27,11 +39,37 @@ public class AdminController {
                 // user should not be allowed!
                 return new ResponseEntity<>("", HttpStatus.FORBIDDEN);
             } else {
-                return new ResponseEntity<>("", HttpStatus.OK);
-                // TODO: Get all users -> with role ROLE_USER
+                Iterable<Users> users = userRepository.findAll();
+
+                // list of ROLE_User and username users
+                List<Pair<Long, String>> userUsers = new ArrayList<>();
+
+                for (Users user: users) {
+                    // user must include ROLE_User
+                    if (user.getRoles().containsKey("ROLE_User")) {
+                        userUsers.add(new Pair<>(user.getUserId(), user.getUsername()));
+                    }
+                }
+
+                // return data
+                List<Map<String, Object>> listUserData = new ArrayList<>();
+
+               // go over each valid user
+               for (Pair<Long, String> pair: userUsers) {
+                   Map<String, Object> currentUserData = new HashMap<>();
+
+                   currentUserData.put("name", pair.getValue());
+                   currentUserData.put("userId", pair.getKey());
+
+                   // Todo: get data from current year -> UserInfo.class
+                    listUserData.add(currentUserData);
+               }
                 // TODO: GET Sick, Glaz, available vacation
                 // TODO: Get requests
                 // TODO: Get Zeitkonto
+                return new ResponseEntity<>(new ObjectMapper().writer().
+                        withDefaultPrettyPrinter()
+                        .writeValueAsString(listUserData), HttpStatus.OK);
             }
         } else {
             return new ResponseEntity<>("", HttpStatus.CONFLICT);
