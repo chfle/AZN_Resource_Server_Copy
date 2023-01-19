8cfa222161e643a882ef298681a8bd3f1d7bc18a
chfle <christian.lehnert.home@gmail.com>
2023-01-19 06:05:09 -0800

    time convert with sql

---
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/controller/WorkTimeController.java
@@ -0,0 +1,77 @@
+package com.lokcenter.AZN_Spring_ResourceServer.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lokcenter.AZN_Spring_ResourceServer.database.repository.UserRepository;
+import com.lokcenter.AZN_Spring_ResourceServer.database.repository.WorkTimeRepository;
+import com.lokcenter.AZN_Spring_ResourceServer.database.tables.Users;
+import com.lokcenter.AZN_Spring_ResourceServer.helper.TimeConvert;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLInetType;
+import com.vladmihalcea.hibernate.type.interval.PostgreSQLIntervalType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+
+/**
+ * Get Worktime related stuff
+ */
+
+@RestController
+@RequestMapping("/worktime")
+public class WorkTimeController {
+    @Autowired
+    private WorkTimeRepository workTimeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    String getSoll(Optional<Users> users) {
+        if (users.isPresent()) {
+            Optional<String> workTime = workTimeRepository.getMostRecentSollByUser(users.get());
+
+            if (workTime.isPresent()) {
+                return workTime.get();
+            }
+        }
+
+        return "";
+    }
+
+    @GetMapping("/soll")
+    @ResponseBody
+    @PreAuthorize("hasAuthority('SCOPE_UserApi.Write')")
+    String getSollTime(@RequestParam(name = "role", required = true) String role,
+                       @RequestParam(name = "userid", required = false) String userid,  Authentication auth) throws JsonProcessingException {
+
+        String soll = "";
+
+        if (userid == null) {
+            Jwt jwt = (Jwt) auth.getPrincipal();
+
+            String name = jwt.getClaim("unique_name");
+
+            // get userId;
+            Optional<Users> user = userRepository.findByUsername(name);
+            soll = getSoll(user);
+
+
+        } else {
+            // user must be admin to use userid
+            if (role.equals("ROLE_Admin")) {
+                Optional<Users> user = userRepository.findById(Long.valueOf(userid));
+                soll = getSoll(user);
+            }
+        }
+
+        return new ObjectMapper().writer().
+                withDefaultPrettyPrinter()
+                .writeValueAsString(soll);
+    }
+}
--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/database/repository/WorkTimeRepository.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/database/repository/WorkTimeRepository.java
@@ -2,17 +2,22 @@ package com.lokcenter.AZN_Spring_ResourceServer.database.repository;
 
 import com.lokcenter.AZN_Spring_ResourceServer.database.tables.Users;
 import com.lokcenter.AZN_Spring_ResourceServer.database.tables.WorkTime;
+import com.vladmihalcea.hibernate.type.interval.PostgreSQLIntervalType;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Time;
 import java.util.Optional;
 
 @Repository
-public interface WorkTimeRepository extends CrudRepository<WorkTime, Long> {
+public interface WorkTimeRepository extends JpaRepository<WorkTime, Long> {
     @Query
     Iterable<WorkTime> findWorkTimeByUsers(Users users);
 
     @Query(value = "select * from work_time where user_id = ?1 ORDER BY date DESC  limit 1", nativeQuery = true)
     Optional<WorkTime> getMostRecentWorkTimeByUser(Users users);
+
+    @Query(value = "select (worktime_end - worktime_start - worktime_pause)\\:\\:varchar as soll from work_time where user_id = ?1 ORDER BY date DESC  limit 1", nativeQuery = true)
+    Optional<String> getMostRecentSollByUser(Users users);
 }
