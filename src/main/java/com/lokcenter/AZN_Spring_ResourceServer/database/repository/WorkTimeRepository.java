83d042d1ed4adc572545c1fcd3e1611c1b10b4bf
chfle <christian.lehnert.home@gmail.com>
2023-01-19 01:12:11 -0800

    dayplan get worktime

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/database/repository/WorkTimeRepository.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/database/repository/WorkTimeRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WorkTimeRepository extends CrudRepository<WorkTime, Long> {
     @Query
     Iterable<WorkTime> findWorkTimeByUsers(Users users);
+
+    @Query(value = "select * from work_time where user_id = ?1 ORDER BY date DESC  limit 1", nativeQuery = true)
+    Optional<WorkTime> getMostRecentWorkTimeByUser(Users users);
 }
