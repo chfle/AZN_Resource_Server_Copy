7c6682ea79786539183f183921f6e76c37833d77
chfle <christian.lehnert.home@gmail.com>
2023-02-17 00:44:55 -0800

    fix null interval

--- src/main/java/com/lokcenter/AZN_Spring_ResourceServer/database/repository/DayPlanDataRepository.java
+++ src/main/java/com/lokcenter/AZN_Spring_ResourceServer/database/repository/DayPlanDataRepository.java
@@ -80,11 +80,11 @@ public interface DayPlanDataRepository extends CrudRepository<DayPlanData, DayPl
     long countByUserIdAndVacationTrue(Long userId);
 
     @Query(value = "select (final_soll + timeV)\\:\\:varchar from (select (weekend_soll + weekday_soll) as final_soll from (select sum(weekdays_soll - soll) as weekday_soll from\n" +
-            "(select  case when sum(worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause)) IS NULL then INTERVAL '0 days' else (sum(worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause)) end as\n" +
+            "(select  case when (worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause) IS NULL then INTERVAL '0 days' else (worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause) end as\n" +
             "weekdays_soll from day_plan_data where user_id=?1 and not (glaz or sick or vacation or school) and EXTRACT(ISODOW FROM set_date) not IN (6, 7)\n" +
             "and extract(year from set_date) = ?2) as dpd cross join (select (worktime_end - worktime_start - worktime_pause)\n" +
             "as soll from work_time where user_id = ?1 ORDER BY work_time.date DESC  limit 1) work_time) as weekdays cross join\n" +
-            "(select  case when (sum(worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause)) IS NULL then INTERVAL '0 days' else (sum(worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause)) endas\n" +
+            "(select  case when (sum(worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause)) IS NULL then INTERVAL '0 days' else (sum(worktime_end - day_plan_data.worktime_start - day_plan_data.worktime_pause)) end as\n" +
             "weekend_soll from day_plan_data where user_id=?1 and not (glaz or sick or vacation or school) and EXTRACT(ISODOW FROM set_date)\n" +
             "IN (6, 7)) as weekend) as soll cross join (select make_interval(0, 0, 0, 0, balance_hours\\:\\:integer, balance_minutes\\:\\:integer, 0)\n" +
             "as timeV from balance where user_id = ?1 and year = ?2) as b", nativeQuery = true)
